b3329d6a2431635f8a146676e88e90cc8f2c1ce5
Ryan Nielson <ryan.nielson@gmail.com>
2026-01-12 23:46:33 -0400

    added instructions

--- app/javascript/controllers/nested_fields_controller.js
+++ app/javascript/controllers/nested_fields_controller.js
@@ -3,12 +3,19 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["ingredients", "ingredientTemplate", "instructions", "instructionTemplate"]
 
+  connect() {
+    this.renumberInstructions()
+  }
+
   addIngredient() {
     this.addFromTemplate(this.ingredientTemplateTarget, this.ingredientsTarget)
+    this.focusLastField(this.ingredientsTarget)
   }
 
   addInstruction() {
     this.addFromTemplate(this.instructionTemplateTarget, this.instructionsTarget)
+    this.renumberInstructions()
+    this.focusLastField(this.instructionsTarget)
   }
 
   addFromTemplate(templateTarget, listTarget) {
@@ -18,5 +25,25 @@ export default class extends Controller {
 
   uniqueKey() {
     return `${Date.now()}${Math.floor(Math.random() * 1000)}`
+  }
+
+  focusLastField(listTarget) {
+    const lastItem = listTarget.lastElementChild
+    if (!lastItem) return
+
+    const field = lastItem.querySelector("input:not([type='hidden']), textarea, select")
+    if (field) field.focus()
+  }
+
+  renumberInstructions() {
+    if (!this.hasInstructionsTarget) return
+    const items = this.instructionsTarget.querySelectorAll("[data-instruction-item]")
+
+    items.forEach((item, index) => {
+      const stepLabel = item.querySelector("[data-step-label]")
+      const positionInput = item.querySelector("input[name$='[position]']")
+      if (stepLabel) stepLabel.textContent = `Step ${index + 1}`
+      if (positionInput) positionInput.value = index + 1
+    })
   }
 }
