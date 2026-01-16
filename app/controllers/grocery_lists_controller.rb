833e83802fd40ca19e00e3b98c2fb41c102e1225
Ryan Nielson <ryan.nielson@gmail.com>
2026-01-15 23:48:48 -0400

    Add grocery lists from meal plan

--- app/controllers/grocery_lists_controller.rb
+++ app/controllers/grocery_lists_controller.rb
@@ -8,34 +8,52 @@ class GroceryListsController < ApplicationController
 
   # GET /grocery_lists/1
   def show
-    @selected_recipes = Current.user.recipes.where(id: @grocery_list.selected_recipe_ids)
+    recipe_id_counts = @grocery_list.selected_recipe_ids.each_with_object(Hash.new(0)) do |recipe_id, counts|
+      counts[recipe_id.to_i] += 1
+    end
+    recipes_by_id = Current.user.recipes.where(id: recipe_id_counts.keys).index_by(&:id)
+    @selected_recipes_with_counts = recipe_id_counts.map do |recipe_id, count|
+      recipe = recipes_by_id[recipe_id]
+      [recipe, count] if recipe.present?
+    end.compact
     @grocery_list_items = @grocery_list.grocery_list_items.order(:name)
   end
 
   # GET /grocery_lists/new
   def new
     load_recipes
+    load_meal_plans
     @grocery_list = Current.user.grocery_lists.new
     @selected_recipe_ids = []
+    @selected_meal_plan_ids = []
     @extra_items_text = ""
   end
 
   # POST /grocery_lists
   def create
     load_recipes
+    load_meal_plans
     @selected_recipe_ids = Array(params[:recipe_ids]).reject(&:blank?)
+    @selected_meal_plan_ids = Array(params[:meal_plan_ids]).reject(&:blank?)
     @extra_items_text = params[:extra_items].to_s
     extra_items = parse_extra_items(@extra_items_text)
     selected_recipes = @recipes.where(id: @selected_recipe_ids)
     @selected_recipe_ids = selected_recipes.pluck(:id)
+    plan_entries = Current.user.meal_plan_entries
+      .includes(recipe: :ingredients)
+      .where(meal_plan_id: @selected_meal_plan_ids)
+    selected_plan_recipe_ids = plan_entries.map(&:recipe_id)
+    selected_recipe_ids = @selected_recipe_ids + selected_plan_recipe_ids
 
     @grocery_list = Current.user.grocery_lists.new(
       name: grocery_list_params[:name],
-      selected_recipe_ids: @selected_recipe_ids
+      selected_recipe_ids: selected_recipe_ids
     )
 
     if @grocery_list.save
-      create_items_from_recipes(selected_recipes)
+      plan_ingredients = plan_entries.flat_map { |entry| entry.recipe.ingredients }
+      recipe_ingredients = selected_recipes.includes(:ingredients).flat_map(&:ingredients)
+      create_items_from_ingredients(plan_ingredients + recipe_ingredients)
       create_extra_items(extra_items)
       redirect_to @grocery_list, notice: "Grocery list was successfully created."
     else
@@ -58,6 +76,14 @@ class GroceryListsController < ApplicationController
       @recipes = Current.user.recipes.order(:name)
     end
 
+    def load_meal_plans
+      week_start = Date.current.beginning_of_week(:monday)
+      @meal_plans = Current.user.meal_plans
+        .includes(:meal_plan_entries)
+        .where("starts_on >= ?", week_start)
+        .order(starts_on: :desc)
+    end
+
     def grocery_list_params
       params.expect(grocery_list: [ :name ])
     end
@@ -66,13 +92,11 @@ class GroceryListsController < ApplicationController
       raw_text.to_s.lines.map(&:strip).reject(&:blank?)
     end
 
-    def create_items_from_recipes(recipes)
-      recipes
-        .includes(:ingredients)
-        .flat_map(&:ingredients)
+    def create_items_from_ingredients(ingredients)
+      ingredients
         .group_by(&:name)
-        .each do |name, ingredients|
-          quantities = ingredients.map { |ingredient| format_quantity(ingredient) }
+        .each do |name, grouped_ingredients|
+          quantities = grouped_ingredients.map { |ingredient| format_quantity(ingredient) }
           @grocery_list.grocery_list_items.create!(
             name: name,
             quantity_text: quantities.join(" · ")
--- test/controllers/grocery_lists_controller_test.rb
+++ test/controllers/grocery_lists_controller_test.rb
@@ -38,6 +38,28 @@ class GroceryListsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to grocery_list_url(grocery_list)
   end
 
+  test "should create grocery list with meal plans and extras" do
+    meal_plan = meal_plans(:one)
+    MealPlanEntry.create!(meal_plan: meal_plan, recipe: recipes(:one), planned_on: meal_plan.starts_on + 1.day)
+
+    assert_difference("GroceryList.count") do
+      assert_difference("GroceryListItem.count", 3) do
+        post grocery_lists_url, params: {
+          grocery_list: { name: "Planned week" },
+          meal_plan_ids: [meal_plan.id],
+          extra_items: "Paper towels"
+        }
+      end
+    end
+
+    grocery_list = GroceryList.last
+    assert_equal 2, grocery_list.selected_recipe_ids.map(&:to_i).count(recipes(:one).id)
+    onion_item = grocery_list.grocery_list_items.find_by(name: "Red Onion")
+    assert onion_item
+    assert_equal 2, onion_item.quantity_text.scan("1 whole").size
+    assert_redirected_to grocery_list_url(grocery_list)
+  end
+
   test "should show grocery list" do
     get grocery_list_url(@grocery_list)
     assert_response :success
